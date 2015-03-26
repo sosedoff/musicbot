160c868b299a564e43468fc5d799f23512aaedd6
Dan Sosedoff <dan.sosedoff@gmail.com>
2015-03-25 23:15:58 -0500

    Print version with -v flag

--- main.go
+++ main.go
@@ -9,11 +9,14 @@ import (
 	"github.com/sosedoff/musicbot/bot"
 )
 
+const version = "0.1.0"
+
 var options struct {
 	MopidyHost   string `long:"mopidy" description:"Mopidy server host:port" env:"MOPIDY_HOST"`
 	SlackToken   string `long:"slack-token" description:"Slack integration token" env:"SLACK_TOKEN"`
 	SlackChannel string `long:"slack-channel" description:"Slack channel name" default:"general" env:"SLACK_CHANNEL"`
 	Debug        bool   `short:"d" long:"debug" description:"Enable debugging mode" default:"false"`
+	Version      bool   `short:"v" long:"version" description:"Print version"`
 }
 
 func init() {
@@ -21,6 +24,11 @@ func init() {
 
 	if err != nil {
 		os.Exit(1)
+	}
+
+	if options.Version {
+		fmt.Printf("musicbot v%s\n", version)
+		os.Exit(0)
 	}
 
 	if options.MopidyHost == "" {
