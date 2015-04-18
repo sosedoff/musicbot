5070a180a844b8f35db2a135ddd4b08006d9f7be
Dan Sosedoff <dan.sosedoff@gmail.com>
2015-04-18 12:47:05 -0500

    Include spotify link in message

--- bot/bot.go
+++ bot/bot.go
@@ -3,6 +3,7 @@ package bot
 import (
 	"fmt"
 	"log"
+	"strings"
 
 	"github.com/sosedoff/musicbot/mopidy"
 	"github.com/sosedoff/musicbot/slack"
@@ -75,8 +76,11 @@ func (bot *Bot) handleMopidyEvents() {
 		case mopidy.TrackPlaybackStarted:
 			data := ev.Data.(mopidy.TrackPlaybackStarted)
 			track := data.TlTrack.Track
+			link := fmt.Sprintf("http://open.spotify.com/track/%s", strings.Split(track.Uri, ":")[2])
+
 			log.Printf("Playing track: %s - %s", track.Uri, track.String())
-			bot.Say(fmt.Sprintf(":musical_note: %s", track.String()))
+			bot.Say(fmt.Sprintf(":musical_note: %s - %s", track.String(), link))
+
 		case mopidy.VolumeChangedEvent:
 			data := ev.Data.(mopidy.VolumeChangedEvent)
 			bot.Say(fmt.Sprintf("Volume is set to %v%s", data.Volume, "%"))
